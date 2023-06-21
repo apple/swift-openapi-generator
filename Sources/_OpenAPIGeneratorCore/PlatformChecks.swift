fc5e7b7c602f19f35ef0f76eea7a921745174e80
Honza Dvorsky <honza@apple.com>
2023-06-21 21:51:27 +0200

    Fix soundness

--- Sources/_OpenAPIGeneratorCore/PlatformChecks.swift
+++ Sources/_OpenAPIGeneratorCore/PlatformChecks.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftOpenAPIGenerator open source project
+//
+// Copyright (c) 2023 Apple Inc. and the SwiftOpenAPIGenerator project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftOpenAPIGenerator project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 // Details: https://github.com/apple/swift-openapi-generator/issues/86
 #if os(iOS) || os(tvOS) || os(watchOS)
