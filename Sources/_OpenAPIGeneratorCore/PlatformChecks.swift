b08a2e5f8d09b1c01d94b7e9b7990d7e33125792
Honza Dvorsky <czechboy0@gmail.com>
2023-06-22 14:11:28 +0200

    Update Sources/_OpenAPIGeneratorCore/PlatformChecks.swift
    Co-authored-by: Si Beaumont <simonjbeaumont@gmail.com>

--- Sources/_OpenAPIGeneratorCore/PlatformChecks.swift
+++ Sources/_OpenAPIGeneratorCore/PlatformChecks.swift
@@ -13,6 +13,6 @@
 //===----------------------------------------------------------------------===//
 
 // Details: https://github.com/apple/swift-openapi-generator/issues/86
-#if os(iOS) || os(tvOS) || os(watchOS)
+#if !(os(macOS) || os(Linux))
 #error("Running the generator tool itself is not supported on iOS, tvOS, and watchOS. Check that your app is not linking the generator directly. For details, check out: https://github.com/apple/swift-openapi-generator/issues/86")
 #endif
