e4b38cb45c772f512f8b1166d91eb1e3315af863
Mahdi Bahrami <github@mahdibm.com>
2023-07-16 09:54:20 +0330

    Update plugin.swift

--- Plugins/OpenAPIGeneratorCommand/plugin.swift
+++ Plugins/OpenAPIGeneratorCommand/plugin.swift
@@ -51,7 +51,9 @@ extension SwiftOpenAPIGeneratorPlugin: CommandPlugin {
         } else {
             let targetNames = try parseTargetNames(arguments: arguments)
             print("Running OpenAPI generator CommandPlugin on select targets: \(targetNames)")
-            targets = try context.package.targets(named: Array(targetNames))
+            targets = try context.package.targets(named: Array(targetNames)).flatMap {
+                [$0] + $0.dependencies.compactMap { $0 as? Target }
+            }
             guard !targets.isEmpty else {
                 throw PluginError.noTargetsMatchingTargetNames(targetNames: Array(targetNames))
             }
@@ -97,7 +99,7 @@ extension SwiftOpenAPIGeneratorPlugin: XcodeCommandPlugin {
         let targets = context.xcodeProject.targets
             .compactMap { $0 as? SourceModuleTarget }
             .filter { targetNames.contains($0.name) }
-            .flatMap { $0.dependencies }
+            .flatMap { [$0] + $0.dependencies.compactMap { $0 as? Target } }
             .compactMap { $0 as? SourceModuleTarget }
         print("Running OpenAPI generator CommandPlugin on targets: \(targets.map(\.name))")
         guard !targets.isEmpty else {
