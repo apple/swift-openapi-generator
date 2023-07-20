2f0ae6287ada9cd1b45bfc6d3293903a028fd965
Honza Dvorsky <honza@apple.com>
2023-07-20 22:01:45 +0200

    Explicitly try to dereference components types, as that isn't done by default

--- Sources/_OpenAPIGeneratorCore/GeneratorPipeline.swift
+++ Sources/_OpenAPIGeneratorCore/GeneratorPipeline.swift
@@ -131,6 +131,22 @@ func makeGeneratorPipeline(
                     // Validate that the document is dereferenceable, which
                     // catches reference cycles, which we don't yet support.
                     _ = try doc.locallyDereferenced()
+                    let components = doc.components
+                    try components.schemas.forEach { schema in
+                        _ = try schema.value.dereferenced(in: components)
+                    }
+                    try components.parameters.forEach { schema in
+                        _ = try schema.value.dereferenced(in: components)
+                    }
+                    try components.headers.forEach { schema in
+                        _ = try schema.value.dereferenced(in: components)
+                    }
+                    try components.requestBodies.forEach { schema in
+                        _ = try schema.value.dereferenced(in: components)
+                    }
+                    try components.responses.forEach { schema in
+                        _ = try schema.value.dereferenced(in: components)
+                    }
 
                     return doc
                 }
