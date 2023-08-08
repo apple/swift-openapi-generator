ce9440dabf1d72940a57f552cf54f86167e26ff4
Honza Dvorsky <honza@apple.com>
2023-08-08 09:59:53 +0200

    Bump the runtime dependency to 0.1.8

--- Package.swift
+++ Package.swift
@@ -78,7 +78,7 @@ let package = Package(
         ),
 
         // Tests-only: Runtime library linked by generated code
-        .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "0.1.7")),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "0.1.8")),
 
         // Build and preview docs
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
