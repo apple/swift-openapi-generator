7b4f72f2862eb4f4f07f5773bd7b83bc9a565746
benfrearson <ben.frearson@sonin.agency>
2023-08-11 16:28:42 +0100

    define initial output

--- Tests/OpenAPIGeneratorReferenceTests/Resources/ReferenceSources/Petstore/Types.swift
+++ Tests/OpenAPIGeneratorReferenceTests/Resources/ReferenceSources/Petstore/Types.swift
@@ -1050,6 +1050,116 @@ public enum Operations {
             case undocumented(statusCode: Int, OpenAPIRuntime.UndocumentedPayload)
         }
     }
+    
+    /// Create a pet using a url form
+    ///
+    /// - Remark: HTTP `POST /pets`.
+    /// - Remark: Generated from `#/paths//pets/post(createPet)`.
+    public enum createPetWithForm {
+        public static let id: String = "createPetWithForm"
+        public struct Input: Sendable, Equatable, Hashable {
+            public struct Path: Sendable, Equatable, Hashable {
+                /// Creates a new `Path`.
+                public init() {}
+            }
+            public var path: Operations.createPetWithForm.Input.Path
+            public struct Query: Sendable, Equatable, Hashable {
+                /// Creates a new `Query`.
+                public init() {}
+            }
+            public var query: Operations.createPetWithForm.Input.Query
+            public struct Headers: Sendable, Equatable, Hashable {
+                public var X_Extra_Arguments: Components.Schemas.CodeError?
+                /// Creates a new `Headers`.
+                ///
+                /// - Parameters:
+                ///   - X_Extra_Arguments:
+                public init(X_Extra_Arguments: Components.Schemas.CodeError? = nil) {
+                    self.X_Extra_Arguments = X_Extra_Arguments
+                }
+            }
+            public var headers: Operations.createPetWithForm.Input.Headers
+            public struct Cookies: Sendable, Equatable, Hashable {
+                /// Creates a new `Cookies`.
+                public init() {}
+            }
+            public var cookies: Operations.createPetWithForm.Input.Cookies
+            @frozen public enum Body: Sendable, Equatable, Hashable {
+                case urlEncodedForm(Components.Schemas.CreatePetRequest)
+            }
+            public var body: Operations.createPetWithForm.Input.Body
+            /// Creates a new `Input`.
+            ///
+            /// - Parameters:
+            ///   - path:
+            ///   - query:
+            ///   - headers:
+            ///   - cookies:
+            ///   - body:
+            public init(
+                path: Operations.createPetWithForm.Input.Path = .init(),
+                query: Operations.createPetWithForm.Input.Query = .init(),
+                headers: Operations.createPetWithForm.Input.Headers = .init(),
+                cookies: Operations.createPetWithForm.Input.Cookies = .init(),
+                body: Operations.createPetWithForm.Input.Body
+            ) {
+                self.path = path
+                self.query = query
+                self.headers = headers
+                self.cookies = cookies
+                self.body = body
+            }
+        }
+        @frozen public enum Output: Sendable, Equatable, Hashable {
+            public struct Created: Sendable, Equatable, Hashable {
+                public struct Headers: Sendable, Equatable, Hashable {
+                    public var X_Extra_Arguments: Components.Schemas.CodeError?
+                    /// Creates a new `Headers`.
+                    ///
+                    /// - Parameters:
+                    ///   - X_Extra_Arguments:
+                    public init(X_Extra_Arguments: Components.Schemas.CodeError? = nil) {
+                        self.X_Extra_Arguments = X_Extra_Arguments
+                    }
+                }
+                /// Received HTTP response headers
+                public var headers: Operations.createPetWithForm.Output.Created.Headers
+                @frozen public enum Body: Sendable, Equatable, Hashable {
+                    case json(Components.Schemas.Pet)
+                }
+                /// Received HTTP response body
+                public var body: Operations.createPetWithForm.Output.Created.Body
+                /// Creates a new `Created`.
+                ///
+                /// - Parameters:
+                ///   - headers: Received HTTP response headers
+                ///   - body: Received HTTP response body
+                public init(
+                    headers: Operations.createPetWithForm.Output.Created.Headers = .init(),
+                    body: Operations.createPetWithForm.Output.Created.Body
+                ) {
+                    self.headers = headers
+                    self.body = body
+                }
+            }
+            /// Successfully created pet
+            ///
+            /// - Remark: Generated from `#/paths//pets/post(createPet)/responses/201`.
+            ///
+            /// HTTP response code: `201 created`.
+            case created(Operations.createPetWithForm.Output.Created)
+            /// Bad request
+            ///
+            /// - Remark: Generated from `#/paths//pets/post(createPet)/responses/400`.
+            ///
+            /// HTTP response code: `400 badRequest`.
+            case badRequest(Components.Responses.ErrorBadRequest)
+            /// Undocumented response.
+            ///
+            /// A response with a code that is not documented in the OpenAPI document.
+            case undocumented(statusCode: Int, OpenAPIRuntime.UndocumentedPayload)
+        }
+    }
     /// - Remark: HTTP `GET /pets/stats`.
     /// - Remark: Generated from `#/paths//pets/stats/get(getStats)`.
     public enum getStats {
