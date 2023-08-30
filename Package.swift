a115af3ebf7ab8e775b8217e8f9a89915385cfb3
Honza Dvorsky <honza@apple.com>
2023-08-30 17:24:14 +0200

    Bump OpenAPIKit - fixes a server variable parsing issue

--- Package.swift
+++ Package.swift
@@ -64,7 +64,7 @@ let package = Package(
         // Read OpenAPI documents
         .package(
             url: "https://github.com/mattpolzin/OpenAPIKit.git",
-            exact: "3.0.0-beta.2"
+            exact: "3.0.0-beta.3"
         ),
         .package(
             url: "https://github.com/jpsim/Yams.git",
