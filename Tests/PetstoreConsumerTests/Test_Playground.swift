463fd730a549940cd7b501065c91fb798eb5eff3
Honza Dvorsky <honza@apple.com>
2023-09-05 11:42:13 +0200

    Add another server streaming example that uses the unfolding initializer on AsyncStream

--- Tests/PetstoreConsumerTests/Test_Playground.swift
+++ Tests/PetstoreConsumerTests/Test_Playground.swift
@@ -216,4 +216,101 @@ final class Test_Playground: XCTestCase {
         let lastResponseChunk = try await responseIterator.next()
         XCTAssertNil(lastResponseChunk)
     }
+
+    func testServerStreaming2() async throws {
+
+        // Server
+
+        struct MyServer: APIProtocol {
+
+            func getStats(
+                _ input: Operations.getStats.Input
+            ) async throws -> Operations.getStats.Output {
+
+                // The server handler sends back the start of the 200 response,
+                // and then sends a few chunks.
+
+                actor ChunkProducer {
+                    private var chunks: [String] = ["hello", "world"]
+
+                    func produceNext() -> HTTPBody.DataType? {
+                        guard !chunks.isEmpty else {
+                            return nil
+                        }
+                        return Array(chunks.removeFirst().utf8)[...]
+                    }
+                }
+                let chunkProducer = ChunkProducer()
+
+                let responseStream = AsyncStream<HTTPBody.DataType>(
+                    unfolding: {
+                        await chunkProducer.produceNext()
+                    }
+                )
+
+                let responseBody = HTTPBody(stream: responseStream, length: .unknown)
+                return .ok(.init(body: .binary(responseBody)))
+            }
+
+            func listPets(_ input: Operations.listPets.Input) async throws -> Operations.listPets.Output {
+                fatalError()
+            }
+
+            func createPet(_ input: Operations.createPet.Input) async throws -> Operations.createPet.Output {
+                fatalError()
+            }
+
+            func postStats(_ input: Operations.postStats.Input) async throws -> Operations.postStats.Output {
+                fatalError()
+            }
+
+            func probe(_ input: Operations.probe.Input) async throws -> Operations.probe.Output {
+                fatalError()
+            }
+
+            func updatePet(_ input: Operations.updatePet.Input) async throws -> Operations.updatePet.Output {
+                fatalError()
+            }
+
+            func uploadAvatarForPet(_ input: Operations.uploadAvatarForPet.Input) async throws
+                -> Operations.uploadAvatarForPet.Output
+            {
+                fatalError()
+            }
+        }
+
+        // Client
+
+        // Create the client.
+        let client: some APIProtocol = MyServer()
+
+        // Send the request, wait for the response.
+        // At this point, both the request and response streams are still open.
+        let response = try await client.getStats(.init())
+
+        // Verify the response status and content type, extract the response stream.
+        guard case .ok(let ok) = response, case .binary(let body) = ok.body else {
+            XCTFail("Unexpected response")
+            return
+        }
+
+        let loggedBody = body.map { chunk in
+            print("Client received a chunk: \(String(decoding: chunk, as: UTF8.self))")
+            return chunk
+        }
+        var responseIterator = loggedBody.makeAsyncIterator()
+
+        // Get a chunk from the response stream, verify the contents.
+        let firstResponseChunk = try await responseIterator.next()
+        XCTAssertEqualStringifiedData(firstResponseChunk, "hello")
+
+        // Get a second chunk.
+        let secondResponseChunk = try await responseIterator.next()
+        XCTAssertEqualStringifiedData(secondResponseChunk, "world")
+
+        // Verify the end of the response stream.
+        let lastResponseChunk = try await responseIterator.next()
+        XCTAssertNil(lastResponseChunk)
+    }
+
 }
