49e360a262cdc69ee02132deda9093d80e41a291
Si Beaumont <beaumont@apple.com>
2023-09-13 10:45:55 +0100

    Add missing license header

--- Tests/OpenAPIGeneratorReferenceTests/CompatabilityTest.swift
+++ Tests/OpenAPIGeneratorReferenceTests/CompatabilityTest.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftOpenAPIGenerator open source project
+//
+// Copyright (c) 2023 Apple Inc. and the SwiftOpenAPIGenerator project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftOpenAPIGenerator project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 import Foundation
 #if canImport(FoundationNetworking)
 import FoundationNetworking
