ce5be924337b59c592c7502fa69f5db07d26dcfa
Si Beaumont <beaumont@apple.com>
2023-09-20 13:40:19 +0100

    Add reference docs to feature flag

--- Sources/_OpenAPIGeneratorCore/FeatureFlags.swift
+++ Sources/_OpenAPIGeneratorCore/FeatureFlags.swift
@@ -29,6 +29,79 @@ public enum FeatureFlag: String, Hashable, Codable, CaseIterable, Sendable {
     // needs to be here for the enum to compile
     case empty
 
+    /// Generate shorthand APIs.
+    ///
+    /// This generates the following syntacic sugar API surface.
+    ///
+    /// #### Streamline operation input parameters
+    ///
+    /// The generated API protocols define one function per OpenAPI operation. These functions take
+    /// a single input parameter that holds all the operation inputs (header fields, query items,
+    /// cookies, body, etc.). Consequently, when making an API call, there is an additional
+    /// initializer to call. This presents unnecessary ceremony, especially when calling operations
+    /// with no parameters or only default parameters.
+    ///
+    /// ```swift
+    /// // before (with parameters)
+    /// _ = try await client.getGreeting(Operations.getGreeting.Input(
+    ///     query: Operations.getGreeting.Input.Query(name: "Maria")
+    /// ))
+    ///
+    /// // before (with parameters, shorthand)
+    /// _ = try await client.getGreeting(.init(query: .init(name: "Maria")))
+    ///
+    /// // before (no parameters, shorthand)
+    /// _ = try await client.getGreeting(.init()))
+    /// ```
+    ///
+    /// When this feature flag is enabled, a protocol extension will be generated, with overload implementations that lift each of the parameters of `Input.init` as function parameters. This removes the need for users to call `Input.init`, which streamlines the API call, especially when the user does not need to provide parameters.
+    ///
+    /// ```swift
+    /// // after (wiih parameters, shorthand)
+    /// _ = try await client.getGreeting(query: .init(name: "Maria"))
+    ///
+    /// // after (no parameters)
+    /// _ = try await client.getGreeting()
+    /// ```
+    ///
+    /// ### Throwing getters for expected responses and content
+    ///
+    /// The generated `Output` type for each API operation is an enum with cases for each documented
+    /// response and a case for an undocumented response. Following this pattern, the `Output.Body`
+    /// is also an enum with cases for every documented content type for the response.
+    ///
+    /// While this API encourages users to handle all possible scenarios, it leads to ceremony when
+    /// the user requires a specific response and receiving anything else is considered an error.
+    /// This is especially apparent for API operations that have just a single response, e.g. `OK`,
+    /// and a single content type, e.g. `application/json`.
+    ///
+    /// ```swift
+    /// // before
+    /// switch try await client.getGreeting() {
+    /// case .ok(let response):
+    ///     switch response.body {
+    ///     case .json(let body):
+    ///         print(body.message)
+    ///     }
+    /// case .undocumented(statusCode: _, _):
+    ///     throw UnexpectedResponseError()
+    /// }
+    /// ```
+    ///
+    /// For users who wish to get an expected response or fail, they will have to define their own error type. They may also make use of `guard case let ... else { throw ... }` which reduces the code, but still presents additional ceremony.
+    ///
+    /// When this feature flag is enabled, a computed property is generated for each enum case. This is a read-only throwing property, which will return the associated value for the expected case, and otherwise throw a runtime error if the value is a different enum case. This allows for expressing the expected outcome as a chained operation.
+    ///
+    /// ```swift
+    /// // after
+    /// print(try await client.getGreeting().ok.body.json.message)
+    /// //                     ^             ^       ^
+    /// //                     |             |       `- (New) Throws if body did not conform to documented JSON.
+    /// //                     |             |
+    /// //                     |             `- (New) Throws if HTTP response is not 200 (OK).
+    /// //                     |
+    /// //                     `- (Existing) Throws if there is an error making the API call.
+    /// ```
     case shorthandAPIs
 }
 
