7cf1e87b5d21ed9afb784ef2cb252a9af1101533
Si Beaumont <beaumont@apple.com>
2023-10-03 07:41:16 +0100

    Update integration test 0.3.0 APIs and temporarily use main

--- IntegrationTest/Package.swift
+++ IntegrationTest/Package.swift
@@ -32,7 +32,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-openapi-generator", .upToNextMinor(from: "0.3.0")),
+        // TODO: When swift-openapi-generator is tagged with 0.3.0, stop depending on main.
+        .package(url: "https://github.com/apple/swift-openapi-generator", /* .upToNextMinor(from: "0.3.0") */ branch: "main"),
         .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "0.3.0")),
     ],
     targets: [
