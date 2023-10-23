536e643f4a41c40a6c64ee4cff15a4442c1faadf
Tibor Bodecs <mail.tib@gmail.com>
2023-10-23 14:00:35 +0200

    client code tutorial updates

--- Sources/swift-openapi-generator/Documentation.docc/Tutorials/_Resources/client.Package.4.swift
+++ Sources/swift-openapi-generator/Documentation.docc/Tutorials/_Resources/client.Package.4.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "GreetingServiceClient",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-openapi-generator", .upToNextMinor(from: "0.3.0")),
