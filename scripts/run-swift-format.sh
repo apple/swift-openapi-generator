f071608012eac47086ac0f9f3cc894db24245327
Honza Dvorsky <honza@apple.com>
2023-10-25 12:31:40 +0200

    Add a soundness --fix flag (#345)
    Add a soundness --fix flag
    
    ### Motivation
    
    When running `./scripts/soundness.sh` produces swift-format warnings, we ask adopters to manually copy/paste a call to swift format to fix the warnings up. This is tedious and unnecessary.
    
    ### Modifications
    
    Add a `--fix` option on the `soundness.sh` script to actually apply the fixes as well, avoiding the need to copy/paste long commands.
    
    ### Result
    
    Easier fixing up of formatting warnings.
    
    ### Test Plan
    
    Manually tested the workflow locally.
    
    
    Reviewed by: glbrntt
    
    Builds:
         ✔︎ pull request validation (5.10) - Build finished. 
         ✔︎ pull request validation (5.8) - Build finished. 
         ✔︎ pull request validation (5.9) - Build finished. 
         ✔︎ pull request validation (compatibility test) - Build finished. 
         ✔︎ pull request validation (docc test) - Build finished. 
         ✔︎ pull request validation (integration test) - Build finished. 
         ✔︎ pull request validation (nightly) - Build finished. 
         ✔︎ pull request validation (soundness) - Build finished. 
    
    https://github.com/apple/swift-openapi-generator/pull/345

--- scripts/run-swift-format.sh
+++ scripts/run-swift-format.sh
@@ -21,12 +21,19 @@ fatal() { error "$@"; exit 1; }
 CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_ROOT="$(git -C "${CURRENT_SCRIPT_DIR}" rev-parse --show-toplevel)"
 
+FORMAT_COMMAND=(lint --strict)
+for arg in "$@"; do
+  if [ "$arg" == "--fix" ]; then
+    FORMAT_COMMAND=(format --in-place)
+  fi
+done
+
 SWIFTFORMAT_BIN=${SWIFTFORMAT_BIN:-$(command -v swift-format)} || fatal "❌ SWIFTFORMAT_BIN unset and no swift-format on PATH"
 
 git -C "${REPO_ROOT}" ls-files -z '*.swift' \
     | grep -z -v -e 'Tests/OpenAPIGeneratorReferenceTests/Resources' \
     -e 'Sources/swift-openapi-generator/Documentation.docc' \
-  | xargs -0 "${SWIFTFORMAT_BIN}" lint --parallel --strict \
+  | xargs -0 "${SWIFTFORMAT_BIN}" "${FORMAT_COMMAND[@]}" --parallel \
   && SWIFT_FORMAT_RC=$? || SWIFT_FORMAT_RC=$?
 
 if [ "${SWIFT_FORMAT_RC}" -ne 0 ]; then
@@ -34,7 +41,7 @@ if [ "${SWIFT_FORMAT_RC}" -ne 0 ]; then
 
   To fix, run the following command:
 
-    % git ls-files -z '*.swift' | grep -z -v -e 'Tests/OpenAPIGeneratorReferenceTests/Resources' -e 'Sources/swift-openapi-generator/Documentation.docc' | xargs -0 swift-format --in-place --parallel
+    % ./scripts/run-swift-format.sh --fix
   "
   exit "${SWIFT_FORMAT_RC}"
 fi
