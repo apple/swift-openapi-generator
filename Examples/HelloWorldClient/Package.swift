b1a2c12b551a2010dd20840aab0217749bfed6e5
Honza Dvorsky <honza@apple.com>
2023-12-01 17:20:53 +0100

    Soundness

--- Examples/HelloWorldClient/Package.swift
+++ Examples/HelloWorldClient/Package.swift
@@ -29,9 +29,7 @@ let package = Package(
                 .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
                 .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
             ],
-            plugins: [
-                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
-            ]
+            plugins: [.plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")]
         )
     ]
 )
