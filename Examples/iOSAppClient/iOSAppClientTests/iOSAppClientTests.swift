de704bbbc9be200de7ad0ecd2a917254002421b5
Honza Dvorsky <honza@apple.com>
2023-12-06 15:24:29 +0100

    Remove more boilerplate

--- Examples/iOSAppClient/iOSAppClientTests/iOSAppClientTests.swift
+++ Examples/iOSAppClient/iOSAppClientTests/iOSAppClientTests.swift
@@ -8,16 +8,6 @@ import XCTest
 
 final class iOSAppClientTests: XCTestCase {
 
-    // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testMockClient() async throws {
         let mockClient = MockClient()
         let response = try await mockClient.getGreeting(query: .init(name: "Test"))
--- Examples/iOSAppClient/iOSAppClientUITests/iOSAppClientUITests.swift
+++ Examples/iOSAppClient/iOSAppClientUITests/iOSAppClientUITests.swift
@@ -7,21 +7,6 @@ import XCTest
 
 final class iOSAppClientUITests: XCTestCase {
 
-    // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testGreeting() throws {
         let app = XCUIApplication()
         // Comment out the following line to have the app running in the simulator try to connect
