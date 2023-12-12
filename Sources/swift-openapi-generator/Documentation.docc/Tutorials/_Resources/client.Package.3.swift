579eb94e88d3f501f2805d2094f9eaf52a1600ba
Honza Dvorsky <honza@apple.com>
2023-12-12 10:58:40 +0100

    PR feedback: include visionOS in all examples and tutorials.

--- Sources/swift-openapi-generator/Documentation.docc/Tutorials/_Resources/client.Package.3.swift
+++ Sources/swift-openapi-generator/Documentation.docc/Tutorials/_Resources/client.Package.3.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "GreetingServiceClient",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)],
     dependencies: [
         .package(url: "https://github.com/apple/swift-openapi-generator", exact: "1.0.0-alpha.1"),
         .package(url: "https://github.com/apple/swift-openapi-runtime", exact: "1.0.0-alpha.1"),
