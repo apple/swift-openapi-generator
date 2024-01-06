f433793cdf12a73aebd355bd95245f8020fabec3
PARAIPAN SORIN <paraipan.sorin1993@gmail.com>
2024-01-06 21:44:48 +0200

    Remove filtered doc

--- Sources/_OpenAPIGeneratorCore/Parser/validateDoc.swift
+++ Sources/_OpenAPIGeneratorCore/Parser/validateDoc.swift
@@ -259,6 +259,11 @@ func validateReferences(in doc: ParsedOpenAPIRepresentation) throws {
 /// - Returns: An array of diagnostic messages representing validation warnings.
 /// - Throws: An error if a fatal issue is found.
 func validateDoc(_ doc: ParsedOpenAPIRepresentation, config: Config) throws -> [Diagnostic] {
+    try validateReferences(in: doc)
+    try validateContentTypes(in: doc) { contentType in
+        (try? _OpenAPIGeneratorCore.ContentType(string: contentType)) != nil
+    }
+
     // Run OpenAPIKit's built-in validation.
     // Pass `false` to `strict`, however, because we don't
     // want to turn schema loading warnings into errors.
@@ -268,13 +273,6 @@ func validateDoc(_ doc: ParsedOpenAPIRepresentation, config: Config) throws -> [
     // block the generator from running.
     // Validation errors continue to be fatal, such as
     // structural issues, like non-unique operationIds, etc.
-    try validateContentTypes(in: doc) { contentType in
-        (try? _OpenAPIGeneratorCore.ContentType(string: contentType)) != nil
-    }
-
-    let filteredDoc = try FilteredDocumentBuilder(document: doc).filter()
-    try validateReferences(in: filteredDoc)
-
     let warnings = try doc.validate(using: Validator().validating(.operationsContainResponses), strict: false)
     let diagnostics: [Diagnostic] = warnings.map { warning in
         .warning(
