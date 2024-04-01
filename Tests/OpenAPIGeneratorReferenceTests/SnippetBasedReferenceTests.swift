03a2878b9bf27ef1d436da2da99b65f0e6e853e8
PARAIPAN SORIN <paraipan.sorin1993@gmail.com>
2024-04-02 00:45:58 +0200

    Add CaseIterable conformance for string, int enums

--- Tests/OpenAPIGeneratorReferenceTests/SnippetBasedReferenceTests.swift
+++ Tests/OpenAPIGeneratorReferenceTests/SnippetBasedReferenceTests.swift
@@ -1189,7 +1189,7 @@ final class SnippetBasedReferenceTests: XCTestCase {
             """,
             """
             public enum Schemas {
-                @frozen public enum MyEnum: String, Codable, Hashable, Sendable {
+                @frozen public enum MyEnum: String, Codable, Hashable, Sendable, CaseIterable {
                     case one = "one"
                     case _empty = ""
                     case _dollar_tart = "$tart"
@@ -1213,7 +1213,7 @@ final class SnippetBasedReferenceTests: XCTestCase {
             """,
             """
             public enum Schemas {
-                @frozen public enum MyEnum: Int, Codable, Hashable, Sendable {
+                @frozen public enum MyEnum: Int, Codable, Hashable, Sendable, CaseIterable {
                     case _0 = 0
                     case _10 = 10
                     case _20 = 20
@@ -1238,7 +1238,7 @@ final class SnippetBasedReferenceTests: XCTestCase {
             """
             public enum Schemas {
                 public struct MyOpenEnum: Codable, Hashable, Sendable {
-                    @frozen public enum Value1Payload: String, Codable, Hashable, Sendable {
+                    @frozen public enum Value1Payload: String, Codable, Hashable, Sendable, CaseIterable {
                         case one = "one"
                         case two = "two"
                     }
@@ -1928,7 +1928,7 @@ final class SnippetBasedReferenceTests: XCTestCase {
             public enum Responses {
                 public struct BadRequest: Sendable, Hashable {
                     public struct Headers: Sendable, Hashable {
-                        @frozen public enum X_hyphen_ReasonPayload: String, Codable, Hashable, Sendable {
+                        @frozen public enum X_hyphen_ReasonPayload: String, Codable, Hashable, Sendable, CaseIterable {
                             case badLuck = "badLuck"
                         }
                         public var X_hyphen_Reason: Components.Responses.BadRequest.Headers.X_hyphen_ReasonPayload?
@@ -2118,7 +2118,7 @@ final class SnippetBasedReferenceTests: XCTestCase {
                     @frozen public enum multipartFormPayload: Sendable, Hashable {
                         public struct logPayload: Sendable, Hashable {
                             public struct Headers: Sendable, Hashable {
-                                @frozen public enum x_hyphen_log_hyphen_typePayload: String, Codable, Hashable, Sendable {
+                                @frozen public enum x_hyphen_log_hyphen_typePayload: String, Codable, Hashable, Sendable, CaseIterable {
                                     case structured = "structured"
                                     case unstructured = "unstructured"
                                 }
