fb69e33e60b0f286479abb44dcf9f512ffe046d8
Jaleel Akbashev <adzhalil@gmail.com>
2024-07-05 08:51:27 +0200

    Back to 2023

--- Examples/bidirectional-event-streams-server-example/Sources/BidirectionalEventStreamsServer/BidirectionalEventStreamsServer.swift
+++ Examples/bidirectional-event-streams-server-example/Sources/BidirectionalEventStreamsServer/BidirectionalEventStreamsServer.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the SwiftOpenAPIGenerator open source project
 //
-// Copyright (c) 2024 Apple Inc. and the SwiftOpenAPIGenerator project authors
+// Copyright (c) 2023 Apple Inc. and the SwiftOpenAPIGenerator project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
