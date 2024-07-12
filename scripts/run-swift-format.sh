fa72f86456f36b7b9ac5cfb94a705ad02fefac71
Franz Busch <f.busch@apple.com>
2024-07-12 12:12:43 +0200

    Use #!/bin/bash

--- scripts/run-swift-format.sh
+++ scripts/run-swift-format.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 ##===----------------------------------------------------------------------===##
 ##
 ## This source file is part of the SwiftOpenAPIGenerator open source project
