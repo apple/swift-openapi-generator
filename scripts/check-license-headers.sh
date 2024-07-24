cd741278e6ef483cf049409a3fa2baa258b98a8b
Franz Busch <f.busch@apple.com>
2024-07-24 12:24:33 +0200

    [GHA] Update to the latest workflows
    # Motivation
    
    I made more progress on the reusable workflows in NIO so it's even easier to adopt them.
    
    # Modification
    
    This PR fixes up the renamed workflow references and adds unit tests and cxx-interop checks.
    
    # Result
    
    Green GH actions CI

--- scripts/check-license-headers.sh
+++ scripts/check-license-headers.sh
@@ -42,6 +42,7 @@ read -ra PATHS_TO_CHECK_FOR_LICENSE <<< "$( \
   ":(exclude).gitignore" \
   ":(exclude).spi.yml" \
   ":(exclude).swift-format" \
+  ":(exclude).licenseignore" \
   ":(exclude).github/*" \
   ":(exclude)CODE_OF_CONDUCT.md" \
   ":(exclude)CONTRIBUTING.md" \
