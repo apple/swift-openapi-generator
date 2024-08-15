28091be98a7ab9d2603ba310a7ee6d86140cafa0
PARAIPAN SORIN <paraipan.sorin1993@gmail.com>
2024-08-15 23:12:06 +0300

    Remove unnecessary text from header

--- Sources/_OpenAPIGeneratorCore/DiagnosticsCollectorProvider.swift
+++ Sources/_OpenAPIGeneratorCore/DiagnosticsCollectorProvider.swift
@@ -12,19 +12,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-/// A feature that can be explicitly enabled before being released.
-///
-/// Commonly used to get early feedback on breaking changes, before
-/// they are enabled by default, which can only be done in a major version.
-///
-/// Once a feature is enabled unconditionally in the next major version,
-/// the corresponding feature flag should be removed at the same time.
-///
-/// For example: a breaking feature is being built while version 0.1 is out,
-/// and is hidden behind a feature flag. Once ready, the feature is
-/// enabled unconditionally on main and the feature flag removed, and version
-/// 0.2 is tagged. (This is for pre-1.0 versioning, would be 1.0 and 2.0 after
-/// 1.0 is released.)
 import Foundation
 
 /// Prepares a diagnostics collector.
