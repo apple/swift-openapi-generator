6593b1b28019be9a9c7e2e470689f3dde89a0736
Honza Dvorsky <honza@apple.com>
2025-01-06 10:53:14 +0100

    Add a CLI option for selecting the naming strategy (#708)
    ### Motivation
    
    The recently introduced naming strategy is only configurable in the
    config file, but pure CLI users might also want to use it without
    creating a config file.
    
    ### Modifications
    
    Add a `--naming-strategy` option to the `swift-openapi-generator` CLI.
    
    ### Result
    
    Users can use the new naming strategy without creating a config file, if
    they didn't have one before.
    
    ### Test Plan
    
    Tested manually on a sample project.

--- Sources/swift-openapi-generator/GenerateOptions.swift
+++ Sources/swift-openapi-generator/GenerateOptions.swift
@@ -33,6 +33,11 @@ struct _GenerateOptions: ParsableArguments {
             "The access modifier to use for the API of generated code. Default: \(Config.defaultAccessModifier.rawValue)"
     ) var accessModifier: AccessModifier?
 
+    @Option(
+        help:
+            "The strategy for converting OpenAPI names into Swift names. Default: \(Config.defaultNamingStrategy.rawValue)"
+    ) var namingStrategy: NamingStrategy?
+
     @Option(help: "Additional import to add to all generated files.") var additionalImport: [String] = []
 
     @Option(help: "Pre-release feature to enable. Options: \(FeatureFlag.prettyListing).") var featureFlag:
@@ -44,6 +49,7 @@ struct _GenerateOptions: ParsableArguments {
 }
 
 extension AccessModifier: ExpressibleByArgument {}
+extension NamingStrategy: ExpressibleByArgument {}
 
 extension _GenerateOptions {
 
@@ -60,10 +66,10 @@ extension _GenerateOptions {
     /// Returns the access modifier requested by the user.
     /// - Parameter config: The configuration specified by the user.
     /// - Returns: The access modifier requested by the user, or nil if the default should be used.
-    func resolvedAccessModifier(_ config: _UserConfig?) -> AccessModifier? {
+    func resolvedAccessModifier(_ config: _UserConfig?) -> AccessModifier {
         if let accessModifier { return accessModifier }
         if let accessModifier = config?.accessModifier { return accessModifier }
-        return nil
+        return Config.defaultAccessModifier
     }
 
     /// Returns a list of additional imports requested by the user.
@@ -78,7 +84,10 @@ extension _GenerateOptions {
     /// Returns the naming strategy requested by the user.
     /// - Parameter config: The configuration specified by the user.
     /// - Returns: The naming strategy requestd by the user.
-    func resolvedNamingStrategy(_ config: _UserConfig?) -> NamingStrategy { config?.namingStrategy ?? .defensive }
+    func resolvedNamingStrategy(_ config: _UserConfig?) -> NamingStrategy {
+        if let namingStrategy { return namingStrategy }
+        return config?.namingStrategy ?? Config.defaultNamingStrategy
+    }
 
     /// Returns the name overrides requested by the user.
     /// - Parameter config: The configuration specified by the user.
