7a271e51e63354a6072c516562211cefbc82f2ae
simonbility <simon.anreiter@wegfinder.at>
2025-06-17 13:28:29 +0200

    Add License Header

--- Examples/type-overrides-example/Sources/ExternalLibrary/PrimeNumber.swift
+++ Examples/type-overrides-example/Sources/ExternalLibrary/PrimeNumber.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftOpenAPIGenerator open source project
+//
+// Copyright (c) 2025 Apple Inc. and the SwiftOpenAPIGenerator project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftOpenAPIGenerator project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 /// Example struct to be used instead of the default generated type.
 /// This illustrates how to introduce a type performing additional validation during Decoding that cannot be expressed with OpenAPI
