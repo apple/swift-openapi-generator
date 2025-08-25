5db200f0054e1edcb6906a79d4d7d197ea8ab357
Paul Toffoloni <paultoffoloni@gmail.com>
2025-08-25 17:01:02 +0200

    Update test

--- Tests/OpenAPIGeneratorReferenceTests/SnippetBasedReferenceTests.swift
+++ Tests/OpenAPIGeneratorReferenceTests/SnippetBasedReferenceTests.swift
@@ -4240,27 +4240,28 @@ final class SnippetBasedReferenceTests: XCTestCase {
                   content:
                     multipart/form-data:
                       schema:
-                        $ref: '#/components/schemas/Node'
+                        $ref: '#/components/schemas/NodeWrapper'
                 responses:
                   default:
                     description: Response
             """,
             """
             schemas:
+              NodeWrapper:
+                type: object
+                properties:
+                  node:
+                    $ref: '#/components/schemas/Node'
               Node:
                 type: object
                 properties:
-                  log: 
-                    type: string
                   parent:
                     $ref: '#/components/schemas/Node'
-                required:
-                  - log
             """,
             input: """
                 public struct Input: Sendable, Hashable {
                     @frozen public enum Body: Sendable, Hashable {
-                        case multipartForm(OpenAPIRuntime.MultipartBody<Components.Schemas.Node>)
+                        case multipartForm(OpenAPIRuntime.MultipartBody<Components.Schemas.NodeWrapper>)
                     }
                     public var body: Operations.post_sol_foo.Input.Body
                     public init(body: Operations.post_sol_foo.Input.Body) {
@@ -4270,22 +4271,45 @@ final class SnippetBasedReferenceTests: XCTestCase {
                 """,
             schemas: """
                 public enum Schemas {
-                    @frozen public enum Node: Sendable, Hashable {
-                        public struct logPayload: Sendable, Hashable {
-                            public var body: OpenAPIRuntime.HTTPBody
-                            public init(body: OpenAPIRuntime.HTTPBody) {
-                                self.body = body
-                            }
-                        }
-                        case log(OpenAPIRuntime.MultipartPart<Components.Schemas.Node.logPayload>)
-                        public struct parentPayload: Sendable, Hashable {
+                    @frozen public enum NodeWrapper: Sendable, Hashable {
+                        public struct nodePayload: Sendable, Hashable {
                             public var body: Components.Schemas.Node
                             public init(body: Components.Schemas.Node) {
                                 self.body = body
                             }
                         }
-                        case parent(OpenAPIRuntime.MultipartPart<Components.Schemas.Node.parentPayload>)
+                        case node(OpenAPIRuntime.MultipartPart<Components.Schemas.NodeWrapper.nodePayload>)
                         case undocumented(OpenAPIRuntime.MultipartRawPart)
+                    }
+                    public struct Node: Codable, Hashable, Sendable {
+                        public var parent: Components.Schemas.Node? {
+                            get  {
+                                self.storage.value.parent
+                            }
+                            _modify {
+                                yield &self.storage.value.parent
+                            }
+                        }
+                        public init(parent: Components.Schemas.Node? = nil) {
+                            self.storage = .init(value: .init(parent: parent))
+                        }
+                        public enum CodingKeys: String, CodingKey {
+                            case parent
+                        }
+                        public init(from decoder: any Decoder) throws {
+                            self.storage = try .init(from: decoder)
+                        }
+                        public func encode(to encoder: any Encoder) throws {
+                            try self.storage.encode(to: encoder)
+                        }
+                        private var storage: OpenAPIRuntime.CopyOnWriteBox<Storage>
+                        private struct Storage: Codable, Hashable, Sendable {
+                            var parent: Components.Schemas.Node?
+                            init(parent: Components.Schemas.Node? = nil) {
+                                self.parent = parent
+                            }
+                            typealias CodingKeys = Components.Schemas.Node.CodingKeys
+                        }
                     }
                 }
                 """,
@@ -4308,31 +4332,15 @@ final class SnippetBasedReferenceTests: XCTestCase {
                             headerFields: &request.headerFields,
                             contentType: "multipart/form-data",
                             allowsUnknownParts: true,
-                            requiredExactlyOncePartNames: [
-                                "log"
-                            ],
+                            requiredExactlyOncePartNames: [],
                             requiredAtLeastOncePartNames: [],
                             atMostOncePartNames: [
-                                "parent"
+                                "node"
                             ],
                             zeroOrMoreTimesPartNames: [],
                             encoding: { part in
                                 switch part {
-                                case let .log(wrapped):
-                                    var headerFields: HTTPTypes.HTTPFields = .init()
-                                    let value = wrapped.payload
-                                    let body = try converter.setRequiredRequestBodyAsBinary(
-                                        value.body,
-                                        headerFields: &headerFields,
-                                        contentType: "text/plain"
-                                    )
-                                    return .init(
-                                        name: "log",
-                                        filename: wrapped.filename,
-                                        headerFields: headerFields,
-                                        body: body
-                                    )
-                                case let .parent(wrapped):
+                                case let .node(wrapped):
                                     var headerFields: HTTPTypes.HTTPFields = .init()
                                     let value = wrapped.payload
                                     let body = try converter.setRequiredRequestBodyAsJSON(
@@ -4341,7 +4349,7 @@ final class SnippetBasedReferenceTests: XCTestCase {
                                         contentType: "application/json; charset=utf-8"
                                     )
                                     return .init(
-                                        name: "parent",
+                                        name: "node",
                                         filename: wrapped.filename,
                                         headerFields: headerFields,
                                         body: body
@@ -4368,42 +4376,24 @@ final class SnippetBasedReferenceTests: XCTestCase {
                     switch chosenContentType {
                     case "multipart/form-data":
                         body = try converter.getRequiredRequestBodyAsMultipart(
-                            OpenAPIRuntime.MultipartBody<Components.Schemas.Node>.self,
+                            OpenAPIRuntime.MultipartBody<Components.Schemas.NodeWrapper>.self,
                             from: requestBody,
                             transforming: { value in
                                 .multipartForm(value)
                             },
                             boundary: contentType.requiredBoundary(),
                             allowsUnknownParts: true,
-                            requiredExactlyOncePartNames: [
-                                "log"
-                            ],
+                            requiredExactlyOncePartNames: [],
                             requiredAtLeastOncePartNames: [],
                             atMostOncePartNames: [
-                                "parent"
+                                "node"
                             ],
                             zeroOrMoreTimesPartNames: [],
                             decoding: { part in
                                 let headerFields = part.headerFields
                                 let (name, filename) = try converter.extractContentDispositionNameAndFilename(in: headerFields)
                                 switch name {
-                                case "log":
-                                    try converter.verifyContentTypeIfPresent(
-                                        in: headerFields,
-                                        matches: "text/plain"
-                                    )
-                                    let body = try converter.getRequiredRequestBodyAsBinary(
-                                        OpenAPIRuntime.HTTPBody.self,
-                                        from: part.body,
-                                        transforming: {
-                                            $0
-                                        }
-                                    )
-                                    return .log(.init(
-                                        payload: .init(body: body),
-                                        filename: filename
-                                    ))
-                                case "parent":
+                                case "node":
                                     try converter.verifyContentTypeIfPresent(
                                         in: headerFields,
                                         matches: "application/json"
@@ -4415,7 +4405,7 @@ final class SnippetBasedReferenceTests: XCTestCase {
                                             $0
                                         }
                                     )
-                                    return .parent(.init(
+                                    return .node(.init(
                                         payload: .init(body: body),
                                         filename: filename
                                     ))
