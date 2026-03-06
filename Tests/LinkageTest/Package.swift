afbaa35632e334c462c3a28a263bf01a5f35997e
Mads Odgaard <mads.odgaard@frameo.com>
2026-03-06 16:50:40 +0100

    linkage test

---
+++ Tests/LinkageTest/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "linkage-test",
+    dependencies: [
+        .package(name: "swift-openapi-generator", path: "../.."),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.11.0", traits: []),
+    ],
+    targets: [
+        .executableTarget(
+            name: "linkageTest",
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime")
+            ],
+            plugins: [.plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")]
+        )
+    ]
+)
