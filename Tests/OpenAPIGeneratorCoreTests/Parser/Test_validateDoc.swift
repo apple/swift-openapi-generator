92deec8cf7af3bd25b837fd2fa03cea37ed9e604
Mathew Polzin <matt.polzin@gmail.com>
2026-05-12 10:30:23 -0500

    Use OpenAPIKit for more validation logic (#880)
    ### Motivation
    
    OpenAPIKit provides some validation help in its latest versions that can
    be used here to reduce the amount of logic this project must maintain.
    For example, the generator need not crawl the document to validate all
    content types, this is the behavior of a
    `Validation<OpenAPIKit.ContentType>`.
    
    ### Modifications
    
    - I've removed the reference validations entirely because they are now
    available in exactly the desired form upstream.
    - I replaced the existing implementation of `ContentType` check with one
    using OpenAPIKit's validation DSL. This mostly serves to avoid the need
    to maintain logic in this library that OpenAPIKit must maintain anyway
    (namely, there's no need to find the `ContentType` values to validate
    them, OpenAPIKit will validate all values of that type anywhere in the
    Document).
    
    ### Result
    
    Removed code, slightly different error messages.
    
    ### Test Plan
    
    Tests have been updated to reflect the new (though comparable) errors.

--- Tests/OpenAPIGeneratorCoreTests/Parser/Test_validateDoc.swift
+++ Tests/OpenAPIGeneratorCoreTests/Parser/Test_validateDoc.swift
@@ -30,16 +30,17 @@ final class Test_validateDoc: Test_Core {
                 "All Operation Ids in Document are unique",
                 "Server Variable\'s enum is either not defined or is non-empty (if defined).",
                 "Server Variable\'s default must exist in enum, if enum is defined.",
-                "JSONSchema reference can be found in components/schemas",
-                "JSONSchema reference can be found in components/schemas",
-                "Response reference can be found in components/responses",
-                "Parameter reference can be found in components/parameters",
-                "Example reference can be found in components/examples",
-                "Request reference can be found in components/requestBodies",
-                "Header reference can be found in components/headers",
-                "Link reference can be found in components/links",
-                "Callbacks reference can be found in components/callbacks",
-                "PathItem reference can be found in components/pathItems", "Operations contain at least one response",
+                "JSONSchema reference points to this document and can be found in components/schemas",
+                "JSONSchema reference points to this document and can be found in components/schemas",
+                "Response reference points to this document and can be found in components/responses",
+                "Parameter reference points to this document and can be found in components/parameters",
+                "Example reference points to this document and can be found in components/examples",
+                "Request reference points to this document and can be found in components/requestBodies",
+                "Header reference points to this document and can be found in components/headers",
+                "Link reference points to this document and can be found in components/links",
+                "Callbacks reference points to this document and can be found in components/callbacks",
+                "PathItem reference points to this document and can be found in components/pathItems",
+                "Operations contain at least one response", "Content type is of form \'<type>/<subtype>\'.",
             ]
         )
     }
@@ -143,11 +144,8 @@ final class Test_validateDoc: Test_Core {
             ],
             components: .noComponents
         )
-        XCTAssertNoThrow(
-            try validateContentTypes(in: doc) { contentType in
-                (try? _OpenAPIGeneratorCore.ContentType(string: contentType)) != nil
-            }
-        )
+        let validator = Validator.blank.validating(contentTypesValidation)
+        XCTAssertNoThrow(try doc.validate(using: validator, strict: false))
     }
 
     func testValidateContentTypes_invalidContentTypesInRequestBody() throws {
@@ -194,15 +192,12 @@ final class Test_validateDoc: Test_Core {
             ],
             components: .noComponents
         )
-        XCTAssertThrowsError(
-            try validateContentTypes(in: doc) { contentType in
-                (try? _OpenAPIGeneratorCore.ContentType(string: contentType)) != nil
-            }
-        ) { error in
-            XCTAssertTrue(error is Diagnostic)
+        let validator = Validator.blank.validating(contentTypesValidation)
+        XCTAssertThrowsError(try doc.validate(using: validator, strict: false)) { error in
+            XCTAssertTrue(error is ValidationErrorCollection)
             XCTAssertEqual(
-                error.localizedDescription,
-                "error: Invalid content type string. [context: contentType=application/, location=/path1/GET/requestBody, recoverySuggestion=Must have 2 components separated by a slash '<type>/<subtype>'.]"
+                OpenAPI.Error(from: error).localizedDescription,
+                "Failed to satisfy: Content type is of form '<type>/<subtype>' at path: .paths['/path1'].get.requestBody.content['application/']"
             )
         }
     }
@@ -251,15 +246,12 @@ final class Test_validateDoc: Test_Core {
             ],
             components: .noComponents
         )
-        XCTAssertThrowsError(
-            try validateContentTypes(in: doc) { contentType in
-                (try? _OpenAPIGeneratorCore.ContentType(string: contentType)) != nil
-            }
-        ) { error in
-            XCTAssertTrue(error is Diagnostic)
+        let validator = Validator.blank.validating(contentTypesValidation)
+        XCTAssertThrowsError(try doc.validate(using: validator, strict: true)) { error in
+            XCTAssertTrue(error is ValidationErrorCollection)
             XCTAssertEqual(
-                error.localizedDescription,
-                "error: Invalid content type string. [context: contentType=/plain, location=/path2/GET/responses, recoverySuggestion=Must have 2 components separated by a slash '<type>/<subtype>'.]"
+                OpenAPI.Error(from: error).localizedDescription,
+                "Failed to satisfy: Content type is of form '<type>/<subtype>' at path: .paths['/path2'].get.responses.200.content['/plain']"
             )
         }
     }
@@ -296,15 +288,12 @@ final class Test_validateDoc: Test_Core {
                 "exampleRequestBody2": .init(content: [.init(rawValue: "image/")!: .content(.init(schema: .string))]),
             ])
         )
-        XCTAssertThrowsError(
-            try validateContentTypes(in: doc) { contentType in
-                (try? _OpenAPIGeneratorCore.ContentType(string: contentType)) != nil
-            }
-        ) { error in
-            XCTAssertTrue(error is Diagnostic)
+        let validator = Validator.blank.validating(contentTypesValidation)
+        XCTAssertThrowsError(try doc.validate(using: validator, strict: false)) { error in
+            XCTAssertTrue(error is ValidationErrorCollection)
             XCTAssertEqual(
-                error.localizedDescription,
-                "error: Invalid content type string. [context: contentType=image/, location=#/components/requestBodies/exampleRequestBody2, recoverySuggestion=Must have 2 components separated by a slash '<type>/<subtype>'.]"
+                OpenAPI.Error(from: error).localizedDescription,
+                "Failed to satisfy: Content type is of form '<type>/<subtype>' at path: .components.requestBodies.exampleRequestBody2.content['image/']"
             )
         }
     }
@@ -345,15 +334,12 @@ final class Test_validateDoc: Test_Core {
                 ),
             ])
         )
-        XCTAssertThrowsError(
-            try validateContentTypes(in: doc) { contentType in
-                (try? _OpenAPIGeneratorCore.ContentType(string: contentType)) != nil
-            }
-        ) { error in
-            XCTAssertTrue(error is Diagnostic)
+        let validator = Validator.blank.validating(contentTypesValidation)
+        XCTAssertThrowsError(try doc.validate(using: validator, strict: false)) { error in
+            XCTAssertTrue(error is ValidationErrorCollection)
             XCTAssertEqual(
-                error.localizedDescription,
-                "error: Invalid content type string. [context: contentType=, location=#/components/responses/exampleRequestBody2, recoverySuggestion=Must have 2 components separated by a slash '<type>/<subtype>'.]"
+                OpenAPI.Error(from: error).localizedDescription,
+                "Failed to satisfy: Content type is of form '<type>/<subtype>' at path: .components.responses.exampleRequestBody2.content."
             )
         }
     }
@@ -421,7 +407,7 @@ final class Test_validateDoc: Test_Core {
                                     )
                                 ])
                             ),
-                            responses: [:],
+                            responses: [200: .response()],
                             callbacks: [.init("Callback"): .a(.component(named: "CallbackReference"))]
                         )
                     )
@@ -447,7 +433,16 @@ final class Test_validateDoc: Test_Core {
                 pathItems: ["Path2Reference": .init()]
             )
         )
-        XCTAssertNoThrow(try validateReferences(in: doc))
+        XCTAssertNoThrow(
+            try validateDoc(
+                doc,
+                config: .init(
+                    mode: .types,
+                    access: Config.defaultAccessModifier,
+                    namingStrategy: Config.defaultNamingStrategy
+                )
+            )
+        )
     }
 
     func testValidateReferences_referenceNotFoundInComponents() throws {
@@ -467,18 +462,27 @@ final class Test_validateDoc: Test_Core {
                                     )
                                 ])
                             ),
-                            responses: [:]
+                            responses: [200: .response()]
                         )
                     )
                 )
             ],
             components: .init(schemas: ["RequestBodyContentSchema": .init(schema: .integer(.init(), .init()))])
         )
-        XCTAssertThrowsError(try validateReferences(in: doc)) { error in
-            XCTAssertTrue(error is Diagnostic)
+        XCTAssertThrowsError(
+            try validateDoc(
+                doc,
+                config: .init(
+                    mode: .types,
+                    access: Config.defaultAccessModifier,
+                    namingStrategy: Config.defaultNamingStrategy
+                )
+            )
+        ) { error in
+            XCTAssertTrue(error is ValidationErrorCollection)
             XCTAssertEqual(
-                error.localizedDescription,
-                "error: Reference not found in components. [context: location=/path/GET/requestBody/content/text/html/schema, reference=#/components/schemas/RequestBodyContentSchemaReference]"
+                OpenAPI.Error(from: error).localizedDescription,
+                "Failed to satisfy: JSONSchema reference points to this document and can be found in components/schemas at path: .paths['/path'].get.requestBody.content['text/html'].schema"
             )
         }
     }
@@ -499,11 +503,20 @@ final class Test_validateDoc: Test_Core {
             ],
             components: .noComponents
         )
-        XCTAssertThrowsError(try validateReferences(in: doc)) { error in
-            XCTAssertTrue(error is Diagnostic)
+        XCTAssertThrowsError(
+            try validateDoc(
+                doc,
+                config: .init(
+                    mode: .types,
+                    access: Config.defaultAccessModifier,
+                    namingStrategy: Config.defaultNamingStrategy
+                )
+            )
+        ) { error in
+            XCTAssertTrue(error is ValidationErrorCollection)
             XCTAssertEqual(
-                error.localizedDescription,
-                "error: External references are not suppported. [context: location=/path/GET/responses/200, reference=ExternalURL]"
+                OpenAPI.Error(from: error).localizedDescription,
+                "Failed to satisfy: Response reference points to this document and can be found in components/responses at path: .paths['/path'].get.responses.200"
             )
         }
     }
