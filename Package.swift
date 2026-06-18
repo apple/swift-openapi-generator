11853921f8745bf585407459c1e40e1fb1dfe5dc
Vladimir Kukushkin <kukushechkin@apple.com>
2026-06-18 14:40:59 +0100

    restore the corss-repo settings, not just the guard

--- Package.swift
+++ Package.swift
@@ -142,6 +142,21 @@ let package = Package(
 // ---    STANDARD CROSS-REPO SETTINGS DO NOT EDIT   --- //
 for target in package.targets {
     switch target.type {
+    case .regular, .test, .executable:
+        var settings = target.swiftSettings ?? []
+        // https://github.com/swiftlang/swift-evolution/blob/main/proposals/0444-member-import-visibility.md
+        settings.append(.enableUpcomingFeature("MemberImportVisibility"))
+        target.swiftSettings = settings
+    case .macro, .plugin, .system, .binary:
+        ()  // not applicable
+    @unknown default:
+        ()  // we don't know what to do here, do nothing
+    }
+}
+// --- END: STANDARD CROSS-REPO SETTINGS DO NOT EDIT --- //
+
+for target in package.targets {
+    switch target.type {
     case .executable, .regular, .test:
         break
     default:
@@ -154,9 +169,6 @@ for target in package.targets {
     // Require `any` for existential types.
     settings.append(.enableUpcomingFeature("ExistentialAny"))
 
-    // https://github.com/swiftlang/swift-evolution/blob/main/proposals/0444-member-import-visibility.md
-    settings.append(.enableUpcomingFeature("MemberImportVisibility"))
-
     if target.name != "PetstoreConsumerTests" {
         // https://github.com/swiftlang/swift-evolution/blob/main/proposals/0409-access-level-on-imports.md
         settings.append(.enableUpcomingFeature("InternalImportsByDefault"))
@@ -164,4 +176,3 @@ for target in package.targets {
 
     target.swiftSettings = settings
 }
-// --- END: STANDARD CROSS-REPO SETTINGS DO NOT EDIT --- //
